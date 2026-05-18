9d86c4cea7065c255cb6822d5c4929a9c40b5614
deupsh <01psh0845@naver.com>
2026-05-18 12:45:59 +0900

    Spring 프로젝트 생성
    Spring 프로젝트 생성

---
+++ Backend/HouseWith-BE/src/main/java/com/housewith/HouseWithBeApplication.java
@@ -0,0 +1,13 @@
+package com.housewith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseWithBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseWithBeApplication.class, args);
+	}
+
+}
---
+++ Backend/HouseWith-BE/src/test/java/com/housewith/HouseWithBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housewith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseWithBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
